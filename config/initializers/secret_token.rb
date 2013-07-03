f3d62a1fd0cfe5fac7393c5419bb3a13d4650b36
Bohdan <bhoshko@gmail.com>
2013-07-03 18:34:23 +0300

    initial commit - proba pera1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dity::Application.config.secret_token = '851283199d64f31764d1f3e89077ea0686e608acda59b737b2929bcf0c7308c14c43d777c7cb9a3e17319142c41608b8b50749b7f7d49425a5d29f4b71057479'
